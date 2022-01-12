9ba3c08e1b505a7c11547a7e6fdb05eea73e9421
jmcarth4@gmail.com <jmcarth4@gmail.com>
2022-01-11 17:21:45 -0700

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,13 @@
+﻿'Jessica McArthur
+'RCET 0265
+'Tim Rossiter
+Module HelloWorld!
+
+    Sub Main()
+        Console.WriteLine("T Swizzle")
+        Console.Beep()
+        Console.WriteLine("Hello Kitty")
+
+    End Sub
+
+End Module
